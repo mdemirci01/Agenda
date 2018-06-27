7649a7edeb2ee819cad6c6bad7d6cfd833b31b5a
Murat Demirci <mdemirci@outlook.com>
2018-06-27 17:35:47 +0300

    ready for layout design

--- Agenda/src/app/app.component.ts
+++ Agenda/src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(private http: HttpClient) { }
+  title = 'app';
   private apiUrl = "https://localhost:44374/api/";
-  title = 'Agenda';
   todoItems: string[] = [];
-   ngOnInit() {
-      this.http.get(this.apiUrl + 'values').subscribe(values => {
-         this.todoItems = values as string[];
-      });
-   }
+  ngOnInit(): void {
+    this.http.get(this.apiUrl + "values").subscribe(data => {
+      this.todoItems = data as string[];
+    });
+  }
 }
