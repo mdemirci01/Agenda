40e07792ff36f01c832e4b385c75fa9ad463873e
Murat Demirci <mdemirci@outlook.com>
2018-06-27 17:44:40 +0300

    design works

--- Agenda/Controllers/TodoListsController.cs
+++ Agenda/Controllers/TodoListsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,13 +8,13 @@ namespace Agenda.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class TodoListsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Liste 1", "Liste 2" };
         }
 
         // GET api/values/5
--- Agenda/src/app/app.component.ts
+++ Agenda/src/app/app.component.ts
@@ -9,10 +9,15 @@ export class AppComponent {
   constructor(private http: HttpClient) { }
   title = 'app';
   private apiUrl = "https://localhost:44374/api/";
+  todoLists: string[] = [];
   todoItems: string[] = [];
   ngOnInit(): void {
-    this.http.get(this.apiUrl + "values").subscribe(data => {
+    this.http.get(this.apiUrl + "todoItems").subscribe(data => {
       this.todoItems = data as string[];
+    });
+
+    this.http.get(this.apiUrl + "todoLists").subscribe(data => {
+      this.todoLists = data as string[];
     });
   }
 }
